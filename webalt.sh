684b7e067d5bdb2e7704c69d320d96171cb84934
Tyler Gannon <tgannon@gmail.com>
2010-12-22 15:52:44 -0800

    Adding in a new script.

--- webalt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo cp -r root/* / && sudo cp webalt.pa/* / && sudo /etc/init.d/httpd restart
-
--- webprod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo cp -r root/* / && sudo cp webprod.pa/* / && sudo /etc/init.d/httpd restart
